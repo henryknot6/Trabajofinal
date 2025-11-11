8f10bb5dd495d564ea9f54d5103378961316d456
SaulSP9 <saulsantospe123@gmail.com>
2025-11-11 10:54:16 -0600

    Añadi 1 tabla de job skills

---
+++ database/migrations/2025_11_11_164950_create_job_skill_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+   Schema::create('job_skill', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('job_id')->constrained()->cascadeOnDelete();
+        $table->foreignId('skill_id')->constrained()->cascadeOnDelete();
+        $table->timestamps();
+        $table->unique(['job_id','skill_id']);
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_skill');
+    }
+};
