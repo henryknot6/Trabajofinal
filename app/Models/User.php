29f3ae0dd405b6b24370040b078b6b9027b6ce01
henryknot6 <henrymendoxa55@gmail.com>
2025-12-07 14:03:16 -0600

    Reorganizacion total:Reinicio de proyecto

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,33 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-use App\Models\Course;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
 
     /**
-     * Los atributos que se pueden asignar de manera masiva.
+     * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
-        // agrega aquí otros campos si tu tabla users tiene más columnas
     ];
 
     /**
-     * Los atributos que deben ocultarse en arrays / JSON.
+     * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $hidden = [
         'password',
@@ -36,59 +34,15 @@ class User extends Authenticatable
     ];
 
     /**
-     * Los atributos que deben castearse.
+     * Get the attributes that should be cast.
      *
-     * @var array<string, string>
+     * @return array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
-
-    /*
-    |--------------------------------------------------------------------------
-    | RELACIONES
-    |--------------------------------------------------------------------------
-    */
-
-    /**
-     * Relación muchos a muchos:
-     * Un usuario puede estar en muchos cursos.
-     */
-    public function courses()
+    protected function casts(): array
     {
-        return $this->belongsToMany(
-            Course::class,      // Modelo Course
-            'usuario_cursos',   // Tabla pivote
-            'user_id',          // FK de usuario en la pivote
-            'course_id'         // FK de curso en la pivote
-        )->withTimestamps();
-    }
-
-    /**
-     * Contactos que este usuario agregó.
-     * (Mis contactos)
-     */
-    public function contactos()
-    {
-        return $this->belongsToMany(
-            self::class,        // Relación con el mismo modelo User
-            'contactos',        // Tabla pivote
-            'usuario_id_1',     // Este usuario
-            'usuario_id_2'      // El contacto
-        )->withTimestamps();
-    }
-
-    /**
-     * Usuarios que me tienen agregado como contacto.
-     */
-    public function contactosDe()
-    {
-        return $this->belongsToMany(
-            self::class,
-            'contactos',
-            'usuario_id_2',     // Este usuario es el contacto
-            'usuario_id_1'      // El que me agregó
-        )->withTimestamps();
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,6 @@
 <?php
 
-use App\Http\Controllers\CursoController;
-
-Route::resource('cursos', CursoController::class);
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
